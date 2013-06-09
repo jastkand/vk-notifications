2ed71f2bc05a99e0ea5cf47af5851633d1ac98ef
Andrey Krivko <jastkand@gmail.com>
2013-06-09 14:15:41 +0700

    remove global API variable

---
+++ libs/api_manager.js
@@ -0,0 +1,13 @@
+var API = {
+    getWall: function(owner_id, access_token, callback) {
+        var requestUrl = "https://api.vk.com/method/wall.get?owner_id=" + owner_id.toString() + "&access_token=" + access_token.toString();
+        $.ajax({
+            url: requestUrl,
+            success: function(data) {
+                if (callback && typeof callback === "function") {
+                    callback(data);
+                }
+            }
+        });
+    }
+};
--- src/api.coffee
+++
@@ -1,8 +0,0 @@
-window.API =
-  getWall: (owner_id, access_token, callback) ->
-    requestUrl = "https://api.vk.com/method/wall.get?owner_id=#{owner_id.toString()}&access_token=#{access_token.toString()}"
-    $.ajax
-      url: requestUrl,
-      success: (data) ->
-        if callback && typeof(callback) == "function"
-          callback data
