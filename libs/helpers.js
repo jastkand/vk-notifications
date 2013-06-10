882c17ea962fc610ffd259541be2c828055799e8
Andrey Krivko <jastkand@gmail.com>
2013-06-11 01:39:27 +0700

    add helpers.js file

---
+++ libs/helpers.js
@@ -0,0 +1,4 @@
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp, "<a href='$1'>$1</a>")
+}
