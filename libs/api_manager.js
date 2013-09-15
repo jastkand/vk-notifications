a4ba9f2f433cd82226238d901f3dd2357500a4bd
Andrey Krivko <jastkand@gmail.com>
2013-09-15 23:54:22 +0700

    remove unused method, remove some repetitive code

--- libs/api_manager.js
+++ libs/api_manager.js
@@ -1,26 +1,7 @@
 var API = {
-    wallGet: function(owner_id, access_token, callback) {
-        var requestUrl = "https://api.vk.com/method/wall.get?owner_id=" + owner_id.toString() + "&access_token=" + access_token.toString();
-        $.ajax({
-            url: requestUrl,
-            success: function(data) {
-                if (callback && typeof callback === "function") {
-                    callback(data);
-                }
-            }
-        });
-    },
     call: function(method, options, callback) {
-        var optionsString, requestUrl;
-        if (options && typeof options === "object") {
-            optionsString = serialize(options);
-        }
-        else {
-            optionsString = options;
-        }
-        requestUrl = "https://api.vk.com/method/" + method.toString() + "?" + optionsString;
         $.ajax({
-            url: requestUrl,
+            url: this.requestUrl(method, options),
             success: function(data) {
                 if (callback && typeof callback === "function") {
                     callback(data);
