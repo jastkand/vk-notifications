ab1da947864cc41c53df54608d23565df87cdba9
Andrey Krivko <jastkand@gmail.com>
2014-05-12 03:22:40 +0700

    add temporary tasks for js watching and building

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var path    = require('path'),
 
 var ROOT_PATH           = __dirname,
     COFFEESCRIPTS_PATH  = path.join(ROOT_PATH, 'javascripts/*.coffee'),
+    JS_SRC_PATH         = path.join(ROOT_PATH, 'javascripts/*.js'),
     SASS_PATH           = path.join(ROOT_PATH, 'stylesheets/**/*.scss'),
     STYLESHEETS_PATH    = path.join(ROOT_PATH, 'public/stylesheets'),
     JAVASCRIPTS_PATH    = path.join(ROOT_PATH, 'public/javascripts');
@@ -25,13 +26,18 @@ function buildCoffee (files) {
        .pipe( gulp.dest(JAVASCRIPTS_PATH) )
 }
 
+function buildJS (files) {
+  files.pipe( include() )
+       .pipe( gulp.dest(JAVASCRIPTS_PATH) )
+}
+
 function buildSlim (files) {
   files.pipe(slim({ pretty: true }))
        .pipe(gulp.dest("public"));
 }
 
-gulp.task('default', ['sass-watch', 'coffee-watch', 'slim-watch', 'vendor-stylesheets']);
-gulp.task('build', ['sass-build', 'coffee-build', 'slim-build', 'vendor-stylesheets']);
+gulp.task('default', ['sass-watch', 'coffee-watch', 'slim-watch', 'js-watch', 'vendor-stylesheets']);
+gulp.task('build', ['sass-build', 'coffee-build', 'slim-build', 'js-build', 'vendor-stylesheets']);
 
 gulp.task('sass-watch', function () {
   gulp.src(SASS_PATH)
@@ -41,6 +47,11 @@ gulp.task('sass-watch', function () {
 gulp.task("coffee-watch", function () {
   gulp.src(COFFEESCRIPTS_PATH)
       .pipe(watch(buildCoffee))
+});
+
+gulp.task("js-watch", function () {
+  gulp.src(JS_SRC_PATH)
+      .pipe(watch(buildJS))
 });
 
 gulp.task('slim-watch', function() {
@@ -54,6 +65,10 @@ gulp.task('sass-build', function () {
 
 gulp.task("coffee-build", function () {
   buildCoffee(gulp.src(COFFEESCRIPTS_PATH))
+});
+
+gulp.task("js-build", function () {
+  buildCoffee(gulp.src(JS_SRC_PATH))
 });
 
 gulp.task('slim-build', function() {
