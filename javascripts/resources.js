91b7811086d81bd764510eb114d95e5fda6c0bb5
Andrey Krivko <jastkand@gmail.com>
2017-02-06 18:50:19 +0600

    Extract AuthPanel as a separate component

--- javascripts/resources.js
+++ javascripts/resources.js
@@ -5,6 +5,7 @@ require('font-awesome/css/font-awesome.css')
 require('emoji/lib/emoji.css')
 require('../stylesheets/styles.scss')
 require('../app/components/Button/styles.scss')
+require('../app/components/AuthPanel/styles.scss')
 require('../app/containers/Popup/styles.scss')
 
 require('../assets/icon-128.png')
