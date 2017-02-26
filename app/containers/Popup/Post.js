fcd28469ba6ee5eeae6eea51a6f713430b1225e0
Andrey Krivko <jastkand@gmail.com>
2017-02-26 17:59:14 +0600

    Fix posts when copy_history object was set

--- app/containers/Popup/Post.js
+++ app/containers/Popup/Post.js
@@ -1,28 +1,39 @@
 import React from 'react'
-import { linkify } from '../../helpers/Text'
-import jEmoji from 'emoji'
+import { processText } from '../../helpers/Text'
 import Attachments from './Attachments'
+import { get } from 'lodash'
 import moment from 'moment'
 
 moment.locale('ru')
+
+class PostContent extends React.Component {
+  render() {
+    return (
+      <div className="text" dangerouslySetInnerHTML={{ __html: processText(this.props.text) }} />
+    )
+  }
+}
 
 export default class Post extends React.Component {
   groupLink(screen_name, item) {
     return `https://vk.com/${screen_name}?w=wall${item.owner_id}_${item.id}`
   }
 
-  processText(content) {
-    let text = content.trim().replace(/\n/g, '<br/>')
-    text = linkify(text)
-    text = text.replace(/\[([^\|]+)\|([^\]]+)\]/gi, '<a href="https://vk.com/$1">$2</a>')
-    return jEmoji.unifiedToHTML(text)
-  }
-
   render() {
     let { group, post } = this.props
     let postLink = this.groupLink(group.screen_name, post)
 
-    console.log(post);
+    let copyHistory
+    let copyHistoryPost = get(post, ['copy_history', 0])
+
+    if (copyHistoryPost) {
+      copyHistory = (
+        <div className="item-quote">
+          <PostContent text={ copyHistoryPost.text } />
+          <Attachments attachments={ copyHistoryPost.attachments } postLink={ postLink } />
+        </div>
+      )
+    }
 
     return (
       <div className="item">
@@ -35,8 +46,9 @@ export default class Post extends React.Component {
           <div className="group-name">
             <a href={ postLink }>{ group.name }</a>
           </div>
-          <div className="text" dangerouslySetInnerHTML={{ __html: this.processText(post.text) }} />
+          <PostContent text={ post.text } />
           <Attachments attachments={ post.attachments } postLink={ postLink } />
+          { copyHistory }
           <span className='datestamp'>{ moment(post.date * 1000).format('LLL') }</span>
         </div>
       </div>
