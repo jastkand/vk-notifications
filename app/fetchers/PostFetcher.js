ad0e7843d090b55c4207fded193a57441084bdb7
Andrey Krivko <jastkand@gmail.com>
2018-04-15 02:21:12 +0600

    Use stored function for getting posts

--- app/fetchers/PostFetcher.js
+++ app/fetchers/PostFetcher.js
@@ -1,34 +1,8 @@
-import { get, sortBy } from 'lodash'
+import { get, chunk, forEach } from 'lodash'
 import { log } from '../helpers/Logger'
 import { call, isAuthError } from '../API'
 import { getGroups } from '../storages/GroupStorage'
 import { getToken, removeToken } from '../storages/SessionStorage'
-
-function normalizePost(post) {
-  log('normalizePost - post', post)
-
-  // If the group doesn't have any posts then post.response.items will be undefined
-  // and calling owner_id will cause the error
-  let groupId = get(post, ['response', 'items', 0, 'owner_id'])
-
-  log('normalizePost - groupId', groupId)
-
-  return { groupId: groupId, totalPostsCount: post.response.count, posts: post.response.items }
-}
-
-function fetchPostsForGroup(groupId, accessToken) {
-  return call('wall.get', { owner_id: groupId, count: 10, access_token: accessToken })
-    .then((response) => {
-      let data = response.data
-
-      if (isAuthError(data)) {
-        removeToken()
-        return Promise.reject({ error: 'auth_error', message: 'Authentication failure', response: data })
-      } else {
-        return Promise.resolve(normalizePost(data))
-      }
-    })
-}
 
 export function fetchAllPosts() {
   return Promise.all([getToken(), getGroups()])
@@ -37,8 +11,53 @@ export function fetchAllPosts() {
         return Promise.reject({ error: 'auth_error', message: 'Access token is missing' })
       }
 
-      let requestPromisses = Object.keys(groups).map((key) => fetchPostsForGroup(key, token))
+      let groupIds = Object.keys(groups)
+      let requestPromises = []
 
-      return Promise.all(requestPromisses)
+      // Maximum API request number in stored functions on VK API
+      let chunkSize = 25;
+      forEach(chunk(groupIds, chunkSize), (groupIdsChunk) => {
+        requestPromises.push(call('execute.getPosts', {
+          group_ids: groupIdsChunk.join(','),
+          access_token: token
+        }))
+      })
+
+      return Promise.all(requestPromises).then((responses) => {
+        if (responses.length === 0) {
+          return []
+        }
+
+        if (isAuthError(responses[0].data)) {
+          removeToken()
+          return Promise.reject({ error: 'auth_error', message: 'Authentication failure', response: responses })
+        } else {
+          return formatQueryResult(responses)
+        }
+      })
     })
+}
+
+function formatQueryResult (responses) {
+  let results = []
+
+  forEach(responses, (responseChunk) => {
+    forEach(responseChunk.data.response, (post) => {
+      results.push(normalizePost(post))
+    })
+  })
+
+  return results
+}
+
+function normalizePost(post) {
+  log('normalizePost - post', post)
+
+  // If the group doesn't have any posts then post.response.items will be undefined
+  // and calling owner_id will cause the error
+  let groupId = get(post, ['items', 0, 'owner_id'])
+
+  log('normalizePost - groupId', groupId)
+
+  return { groupId: groupId, totalPostsCount: post.count, posts: post.items }
 }
