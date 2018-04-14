b4fe30debb98ffd5a80024243c94fa127ad85a45
Andrey Krivko <jastkand@gmail.com>
2018-04-14 23:55:23 +0600

    Move links handler to Feed component

--- app/components/Feed.js
+++ app/components/Feed.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import $ from 'jquery'
 import Post from './Post'
 import styles from './Feed.css'
 
 export default class Feed extends React.Component {
+  componentWillMount () {
+    $(document).on('click', 'a', function (e) {
+      let href = $(this).attr('href')
+
+      if (href) {
+        chrome.tabs.create({ url: href, selected: true });
+      }
+
+      e.preventDefault();
+    })
+  }
+
   render() {
     let postNodes = this.props.posts.map((post) => {
       let group = this.props.groups[post.owner_id]
