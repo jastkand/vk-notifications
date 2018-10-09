c221ac34ab650278734c88ff0b7201fab8aed0e6
Andrey Krivko <jastkand@gmail.com>
2018-10-10 04:02:26 +0600

    Upgrade font-awesome
    # Conflicts:
    #	package.json
    #	yarn.lock

--- app/components/HeaderButton.js
+++ app/components/HeaderButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './HeaderButton.css'
 
 export default (props) => {
