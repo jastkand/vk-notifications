935b88e80a9b96564055793deae2ebfbc1a4eaa6
Andrey Krivko <jastkand@gmail.com>
2018-10-10 04:01:19 +0600

    Remove usage of axios from API

--- app/API.js
+++ app/API.js
@@ -1,28 +1,26 @@
-import axios from 'axios'
-
-function serialize(obj) {
-    var str = [];
-    for(var p in obj)
-        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    return str.join("&");
+function serialize (obj) {
+  let str = []
+  for (let p in obj) {
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]))
+  }
+  return str.join("&")
 }
 
 export function call(method, params) {
-  return axios.post(requestUrl(method, params))
+  return fetch(requestUrl(method, params), { method: 'post' })
+    .then(res => res.json())
 }
 
-function requestUrl(method, params) {
-  var paramsString;
+function requestUrl (method, params) {
+  let paramsString;
   if (params && typeof params === "object") {
-    Object.assign(params, {v: '5.62'});
-    paramsString = serialize(params)
-  }
-  else {
+    paramsString = serialize(Object.assign({}, params, { v: '5.62' }))
+  } else {
     paramsString = params
   }
   return `https://api.vk.com/method/${method.toString()}?${paramsString}`
 }
 
-export function isAuthError(response) {
+export function isAuthError (response) {
   return response && response.error && response.error.error_code == 5
 }
