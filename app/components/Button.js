a36f44205a4981bfaca4bc506cf14d0a6052560a
Andrey Krivko <jastkand@gmail.com>
2019-06-06 23:23:24 +0600

    Use button element for button component

--- app/components/Button.js
+++ app/components/Button.js
@@ -3,10 +3,15 @@ import styles from './Button.css'
 
 export default class Button extends React.Component {
   render() {
-    let btnClass = `${styles.btn} ${styles[this.props.className]}`
+    let modifierClass = styles[this.props.className]
+    let btnClass = `${styles.btn}`
+
+    if (modifierClass) {
+      btnClass = `${btnClass} ${modifierClass}`
+    }
 
     return (
-      <a onClick={ this.props.onClick } className={ btnClass }>{ this.props.children }</a>
+      <button onClick={ this.props.onClick } className={ btnClass }>{ this.props.children }</button>
     )
   }
 }
