55e7e1d27a520e0e1bcc9097936147e0cde3ae14
Andrey Krivko <jastkand@gmail.com>
2019-06-06 23:26:33 +0600

    Tweaks and fixes in background file

--- app/API.js
+++ app/API.js
@@ -1,20 +1,31 @@
-function serialize (obj) {
+const apiVersion = '5.95'
+
+function buildQueryParams (obj) {
   let str = []
   for (let p in obj) {
-    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]))
+    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
   }
-  return str.join("&")
+  return str.join('&')
 }
 
-export function call(method, params) {
-  return fetch(requestUrl(method, params), { method: 'post' })
-    .then(res => res.json())
+export const call = async (method, params) => {
+  console.group('API.call')
+  const url = requestUrl(method, params);
+  console.log({ url })
+
+  const response = await fetch(url, { method: 'POST' })
+  console.groupEnd()
+
+  return response.json()
 }
 
 function requestUrl (method, params) {
   let paramsString;
-  if (params && typeof params === "object") {
-    paramsString = serialize(Object.assign({}, params, { v: '5.62' }))
+  if (params && typeof params === 'object') {
+    paramsString = buildQueryParams({
+      ...params,
+      v: apiVersion,
+    })
   } else {
     paramsString = params
   }
