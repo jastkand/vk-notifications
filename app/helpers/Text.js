0d063521deeb42cd24e918393b129fcfa82683e8
Andrey Krivko <jastkand@gmail.com>
2019-07-28 00:19:32 +0600

    Use autolinker to parse links in the content

--- app/helpers/Text.js
+++ app/helpers/Text.js
@@ -1,19 +1,11 @@
-import jEmoji from 'emoji'
+import Autolinker from 'autolinker'
 
 function linkify(text) {
-  var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim,
-    pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim,
-    emailAddressPattern = /[\w.+-]+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gim;
-
-  return text
-    .replace(urlPattern, '<a href="$&">$&</a>')
-    .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
-    .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+  return Autolinker.link(text, {})
 }
 
 export function processText(content) {
   let text = content.trim().replace(/\n/g, '<br/>')
   text = linkify(text)
-  text = text.replace(/\[([^\|]+)\|([^\]]+)\]/gi, '<a href="https://vk.com/$1">$2</a>')
-  return jEmoji.unifiedToHTML(text)
+  return text.replace(/\[([^\|]+)\|([^\]]+)\]/gi, '<a href="https://vk.com/$1">$2</a>')
 }
