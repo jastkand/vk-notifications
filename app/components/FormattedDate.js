3ee86e7a6babe0d0343c18b6a5cfd2347a7f7b86
Andrey Krivko <jastkand@gmail.com>
2021-05-24 12:33:11 +0600

    Fix date-fns bad format

--- app/components/FormattedDate.js
+++ app/components/FormattedDate.js
@@ -4,7 +4,7 @@ import React from 'react'
 import styles from './FormattedDate.css'
 
 export default (props) => {
-  let formattedDate = format(new Date(props.date * 1000), 'D MMMM, YYYY HH:mm', { locale: ru })
+  let formattedDate = format(new Date(props.date * 1000), 'd MMMM, yyyy HH:mm', { locale: ru })
 
   return (
     <div className={ styles.datestamp }>{ formattedDate }</div>
